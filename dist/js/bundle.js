e2dddbbc0e915baeb71b237818435dff1b8bbe9b
Jacob Aintablian <jacob.aintablian@gmail.com>
2017-05-28 20:25:22 -0600

    best

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -26,12 +26,35 @@ angular.module('noserv').controller('ctrl', function ($scope, service) {
 $(document).ready(function () {
 
     $("#dialog").dialog({
-        autoOpen: false,
-        height: 400,
-        width: 500
+        autoOpen: false
+
     });
     $("#opener").click(function () {
         $("#dialog").dialog("open");
+    });
+
+    $('#addBoar').click(function () {
+        $('#aImage').html('<img src="./img/boarf.gif">');
+    });
+    $('#addRhino').click(function () {
+        $('#aImage').html('<img src="./img/rhinof.gif">');
+    });
+    $('#clear').click(function () {
+        $('#aImage').html('');
+    });
+    $('#addBoar2').click(function () {
+        $('#bImage').html('<img src="./img/boarf.gif">');
+    });
+    $('#addRhino2').click(function () {
+        $('#bImage').html('<img src="./img/rhinof.gif">');
+    });
+    $('#clear2').click(function () {
+        $('#bImage').html('');
+    });
+
+    $('#fight').click(function () {
+        $('#bImage').append(' GYŐZELEM ');
+        $('#aImage').append(' VERESÉG ');
     });
 });
 'use strict';
--- js/jquery.js
+++ js/jquery.js
@@ -4,10 +4,37 @@ $(document).ready(function(){
 
     $("#dialog").dialog({
         autoOpen: false,
-        height: 400,
-        width: 500
+        
     });
     $("#opener").click(function () {
         $("#dialog").dialog("open");
+     
     });
+
+    $('#addBoar').click(function(){
+        $('#aImage').html('<img src="./img/boarf.gif">')
+    });
+    $('#addRhino').click(function(){
+        $('#aImage').html('<img src="./img/rhinof.gif">')
+    });
+    $('#clear').click(function(){
+        $('#aImage').html('')
+    });
+    $('#addBoar2').click(function(){
+        $('#bImage').html('<img src="./img/boarf.gif">')
+    });
+    $('#addRhino2').click(function(){
+        $('#bImage').html('<img src="./img/rhinof.gif">')
+    });
+    $('#clear2').click(function(){
+        $('#bImage').html('')
+    });
+
+    $('#fight').click(function(){
+        $('#bImage').append(' GYŐZELEM ');
+        $('#aImage').append(' VERESÉG ');
+    });
+
+
+
 });
